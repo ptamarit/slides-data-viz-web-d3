0497250086a8a884861dad3a69a7800a077539fe
Pablo Tamarit <pablo.tmrt@gmail.com>
2013-11-09 20:42:20 +0100

    Load all the grunt tasks automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,9 @@ module.exports = function(grunt) {
     '<%= pkg.homepage %>'
   ];
 
+  // Load all the grunt tasks.
+  require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -197,22 +200,6 @@ module.exports = function(grunt) {
       src: '**/*'
     }
   });
-
-  grunt.loadNpmTasks('grunt-jsonlint');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-html-validation');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-usemin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-rev');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-text-replace');
-  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('check', ['jsonlint', 'jshint', 'csslint', 'validation']);
 
